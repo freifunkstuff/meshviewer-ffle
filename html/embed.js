1c93d924dea102eec8a6d5693b5d3192c7678f3e
Michael Wyraz <michael.wyraz@evermind.de>
2021-12-02 21:07:25 +0100

    Fix irame message handling

--- html/embed.js
+++ html/embed.js
@@ -11,22 +11,17 @@
     }
 
     function updateIframeHash() { // see https://gist.github.com/manufitoussi/7529fa882ff0b737f257
-        if(iframe.contentWindow.location.host !== "") {
-            // iframe already loaded.
-            iframe.contentWindow.location.hash = window.location.hash;
-          } else {
-            // iframe is just starting.
-            var newHash = window.location.hash;
-            var srcStr = iframe.getAttribute('src');
-            var words = srcStr.split('#');
-            var href = words[0];
-            var newSrc = href + newHash;
-            iframe.setAttribute('src', newSrc);
-          }
+        // iframe is just starting.
+        var newHash = window.location.hash;
+        var srcStr = iframe.getAttribute('src');
+        var words = srcStr.split('#');
+        var href = words[0];
+        var newSrc = href + newHash;
+        iframe.setAttribute('src', newSrc);
     };
 
     updateIframeHash();
-    iframe.contentWindow.addEventListener("message", (event) => {
+    window.addEventListener("message", (event) => {
         if (event && event.data && event.data.hash) {
             window.location.replace(event.data.hash);
         }
